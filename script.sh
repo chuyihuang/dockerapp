61036ccaeac07b198d9678c0491f2a3a9c7383b3
chuyihuang <chuyihuang@gmail.com>
2015-11-21 19:14:15 +0800

    裝mysql2 gem

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 export APP_PWD=password
 
-db() {
-  # docker run -P --volumes-from app_data --name app_db -e POSTGRES_USER=app_user -e POSTGRES_PASSWORD=$APP_PWD -d -t postgres:latest
-  # docker run -e POSTGRES_USER=app_user -e POSTGRES_PASSWORD=$APP_PWD -d -t postgres:latest
-}
+# db() {
+#   # docker run -P --volumes-from app_data --name app_db -e POSTGRES_USER=app_user -e POSTGRES_PASSWORD=$APP_PWD -d -t postgres:latest
+#   # docker run -e POSTGRES_USER=app_user -e POSTGRES_PASSWORD=$APP_PWD -d -t postgres:latest
+# }
 
 app() {
   docker stop app
